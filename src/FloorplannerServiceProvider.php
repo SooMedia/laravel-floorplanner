dd5d3db939c958b8ce727dc04a5c1cdcc2d9c52c
Willem Oostendorp <willem@soomedia.nl>
2018-09-28 14:19:31 +0200

    Add the service provider and config file

---
+++ src/FloorplannerServiceProvider.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace SooMedia\LaravelFloorplanner;
+
+use Illuminate\Foundation\Application;
+use Illuminate\Support\ServiceProvider;
+use SooMedia\Floorplanner\FloorplannerClient;
+
+/**
+ * Class FloorplannerServiceProvider
+ *
+ * @package SooMedia\LaravelFloorplanner
+ */
+class FloorplannerServiceProvider extends ServiceProvider
+{
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = true;
+
+    /**
+     * Register bindings in the container.
+     *
+     * @return void
+     */
+    public function register(): void
+    {
+        $this->app->bind(
+            FloorplannerClient::class,
+            function (Application $app) {
+                $config = $app['config']['floorplanner'];
+
+                return new FloorplannerClient(
+                    $config['api_key'],
+                    $config['httpClientOptions'] ?? []
+                );
+            }
+        );
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot(): void
+    {
+        $this->publishes([
+            __DIR__ . '/../config/floorplanner.php' => config_path('floorplanner.php'),
+        ]);
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides(): array
+    {
+        return [
+            FloorplannerClient::class,
+        ];
+    }
+}
