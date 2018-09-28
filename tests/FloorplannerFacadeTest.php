e8ad1d4c5ffd842e4135b052701902094264b504
Willem Oostendorp <willem@soomedia.nl>
2018-09-28 14:20:46 +0200

    Add tests for the service provider and facade

---
+++ tests/FloorplannerFacadeTest.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Tests;
+
+use Orchestra\Testbench\TestCase;
+use SooMedia\Floorplanner\Endpoints\UsersEndpoint;
+use SooMedia\LaravelFloorplanner\FloorplannerFacade;
+use SooMedia\LaravelFloorplanner\FloorplannerServiceProvider;
+
+/**
+ * Class FloorplannerFacadeTest
+ *
+ * @package Tests
+ * @coversDefaultClass \SooMedia\LaravelFloorplanner\FloorplannerFacade
+ */
+class FloorplannerFacadeTest extends TestCase
+{
+    /**
+     * Get package providers.
+     *
+     * @param  \Illuminate\Foundation\Application $app
+     * @return array
+     * @SuppressWarnings("unused")
+     */
+    protected function getPackageProviders($app): array
+    {
+        return [
+            FloorplannerServiceProvider::class,
+        ];
+    }
+
+    /**
+     * Define environment setup.
+     *
+     * @param  \Illuminate\Foundation\Application $app
+     * @return void
+     */
+    protected function getEnvironmentSetUp($app): void
+    {
+        $app['config']->set('floorplanner.api_key', 'my_api_key');
+    }
+
+    /**
+     * Get package aliases.
+     *
+     * @param  \Illuminate\Foundation\Application $app
+     * @return array
+     * @SuppressWarnings("unused")
+     */
+    protected function getPackageAliases($app)
+    {
+        return [
+            'Floorplanner' => FloorplannerFacade::class,
+        ];
+    }
+
+    /**
+     * @covers ::getFacadeAccessor
+     */
+    public function testFacade(): void
+    {
+        app('config')->set(['floorplanner.api_key' => 'my_api_key']);
+
+        $users = \Floorplanner::users();
+
+        $this->assertInstanceOf(UsersEndpoint::class, $users);
+    }
+}
---
+++ tests/FloorplannerServiceProviderTest.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Tests;
+
+use Orchestra\Testbench\TestCase;
+use SooMedia\Floorplanner\FloorplannerClient;
+use SooMedia\LaravelFloorplanner\FloorplannerServiceProvider;
+
+/**
+ * Class FloorplannerServiceProviderTest
+ *
+ * @package Tests
+ * @coversDefaultClass \SooMedia\LaravelFloorplanner\FloorplannerServiceProvider
+ */
+class FloorplannerServiceProviderTest extends TestCase
+{
+    /**
+     * Get package providers.
+     *
+     * @param  \Illuminate\Foundation\Application $app
+     * @return array
+     * @SuppressWarnings("unused")
+     */
+    protected function getPackageProviders($app): array
+    {
+        return [
+            FloorplannerServiceProvider::class,
+        ];
+    }
+
+    /**
+     * Define environment setup.
+     *
+     * @param  \Illuminate\Foundation\Application $app
+     * @return void
+     */
+    protected function getEnvironmentSetUp($app): void
+    {
+        $app['config']->set('floorplanner.api_key', 'my_api_key');
+    }
+
+    /**
+     * @covers ::register
+     * @covers ::boot
+     * @covers ::provides
+     */
+    public function testBinding(): void
+    {
+        $client = app(FloorplannerClient::class);
+
+        $this->assertNotEmpty($client);
+        $this->assertInstanceOf(FloorplannerClient::class, $client);
+    }
+}
